7c5851b6844280d66faad0b61dcf053d4fdb42b1
Jon Leithe <jon@jonleithe.no>
2024-01-20 14:52:54 +0100

    Added support for using system date and time

--- astronomy.cpp
+++ astronomy.cpp
@@ -13,6 +13,8 @@
 #include <iomanip>
 #include <getopt.h>
 #include <locale.h>
+#include <time.h>
+#include <sys/time.h>
 
 using namespace std;
 
@@ -26,7 +28,8 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-	bool	verbose = false;
+	bool	verbose = false, timeSupplied = false;
+	bool	dateSupplied = false;
 	int		year, month, day, hour, minute, second, timezone, dst;
 	double	lat, lon;
 
@@ -57,15 +60,15 @@ int main(int argc, char *argv[])
 
 	//	Initialise command line variables to some default values, should the user fail to provide...
 	//	This is for development purpose only, not to enter production code.
-	year		= 2023;
-	month		= 9;
-	day			= 25;
-	hour		= 13;
-	minute		= 0;
-	second		= 0;
+	//year		= 2024;
+	//month		= 1;
+	//day			= 21;
+	//hour		= 12;
+	//minute		= 0;
+	//second		= 0;
 	timezone	= 1;
-	lat			= 64.68059;	//	Oppi Leite
-	lon			= 11.29153;	//	Oppi Leite
+	lat			= 64.68055000;	//	Oppi Leite
+	lon			= 11.29132222;	//	Oppi Leite
 	dst			= 0;
 
 
@@ -87,6 +90,7 @@ int main(int argc, char *argv[])
 								parseOptarg(&optarg);
 								day = atoi(optarg);
 								// end of parsing optarg
+								dateSupplied = true;
 
 								break;
 							}
@@ -97,6 +101,7 @@ int main(int argc, char *argv[])
 								parseOptarg(&optarg);
 								second = atoi(optarg);
 								// end of parsing optarg
+								timeSupplied = true;
 
 								break;
 							}
@@ -127,16 +132,40 @@ int main(int argc, char *argv[])
 							}
 				case 'h':	{
 								printUsage();
-								//cout << "Add some useful information on how to use this software..." << endl;
 								exit(1);
 							}
 				default:	{
-								printf("switch default\n");
+								cout << "Switch default\n" << endl;
 								break;
 							}
 			}	//end of getopt() switch statement
 		}	// end of while-loop
 
+	if(!dateSupplied){
+		cout << "Date not supplied, using system date..." << endl;
+
+		time_t current = time(NULL);
+		struct tm* current_s = localtime(&current);
+
+		day		= current_s->tm_mday;
+		month	= ++current_s->tm_mon;
+		year	= 1900 + current_s->tm_year;
+
+		cout << "Day: " << day << ", month: " << month << ", year: " << year << endl;
+	}
+
+	if(!timeSupplied){
+			cout << "Time not supplied, using system time..." << endl;
+
+			time_t current = time(NULL);
+			struct tm* current_s = localtime(&current);
+
+			hour	= current_s->tm_hour;
+			minute	= current_s->tm_min;
+			second	= current_s->tm_sec;
+
+			cout << "Time: " << hour << ":" << minute << ":" << second << endl;
+		}
 
 
 	aSun *sun = new aSun(year, month, day, hour, minute, second, timezone, lat, lon, dst, verbose);
